dc81764e6434251c09b3def979d810c0a4f5ae4a
Naveen Gupta <141839367+naveeng0602@users.noreply.github.com>
2023-11-05 00:39:34 +0530

    update

--- App.js
+++ App.js
@@ -1,24 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './screens/Home';
+import About from './screens/About';
+
+
+//routes
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
-<View style={styles.container}>
-      <Text>Naveen</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Stack.Navigator>
+        <Stack.Screen name="home" component={Home} options={{
+          headerShown: false,
+        }}/>
+        <Stack.Screen name = 'About' component={About}
+      </Stack.Navigator>
     </NavigationContainer>
     
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
